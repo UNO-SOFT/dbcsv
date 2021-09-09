183cf5d3fc72a4ace87655c036c88f45cce7bf47
Tamás Gulácsi <T.Gulacsi@unosoft.hu>
2021-09-09 08:36:39 +0200

    globalcontext.Wrap: Less signals and os.Exit adter 5s

--- globalctx.go
+++ globalctx.go
@@ -21,18 +21,23 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
 // Wrap returns a new context with cancel that is canceled on interrupts.
 func Wrap(ctx context.Context) (context.Context, context.CancelFunc) {
 	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM, syscall.SIGINT, syscall.SIGHUP)
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
 	var cancel context.CancelFunc
 	ctx, cancel = context.WithCancel(ctx)
 	go func() {
 		<-sigCh
 		signal.Stop(sigCh)
 		cancel()
+		go func() {
+			time.Sleep(5 * time.Second)
+			os.Exit(1)
+		}()
 	}()
 	return ctx, cancel
 }
